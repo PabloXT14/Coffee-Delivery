d62906b407015fcec45da7152d76e03f288cf0fa
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2022-09-23 23:06:00 -0300

    feat: implemented Add Coffee ro Cart reduce

--- src/reducers/cart-items/reducer.ts
+++ src/reducers/cart-items/reducer.ts
@@ -1,3 +1,4 @@
+import { produce } from 'immer'
 import { Coffee } from '../../pages/Home/components/CoffeeCard'
 import { ActionTypes } from './actions'
 
@@ -13,18 +14,18 @@ export function cartItemsReducer(state: CartItemsState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_COFFEE_TO_CART: {
       const coffeeAlreadyExistsInCart = state.cartItems.findIndex(
-        (cartItem) => cartItem.id === action.payloadcoffee.id,
+        (cartItem) => cartItem.id === action.payload.newCoffee.id,
       )
 
-      // const newCart = produce(cartItems, (draft) => {
-      //   if (coffeeAlreadyExistsInCart < 0) {
-      //     draft.push(coffee)
-      //   } else {
-      //     draft[coffeeAlreadyExistsInCart].quantity += coffee.quantity
-      //   }
-      // })
-
-      // setCartItems(newCart)
+      const newCartItemsState = produce(state, (draft) => {
+        if (coffeeAlreadyExistsInCart < 0) {
+          draft.cartItems.push(action.payload.newCoffee)
+        } else {
+          draft.cartItems[coffeeAlreadyExistsInCart].quantity += action.payload.newCoffee.quantity
+        }
+      })
+      
+      return newCartItemsState;
     }
   }
 }
